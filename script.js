561ee6928927e9bc3a2e03aa6e8b41afbc432842
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-06-03 15:20:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let button=document.getElementById("button");
+let color=document.getElementById("colorSelect")
+button.addEventListener("click",func)
+function func(){
+	color.remove(color.value)
+}
