ffbc745b90dcb49f02ebeba8948993c19fb29724
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-06-03 17:15:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 let button=document.getElementById("button");
 let color=document.getElementById("colorSelect")
 button.addEventListener("click",func)
-function func(){
-	color.remove(color.value)
+function func() {
+    for(let i = 0; i < color.options.length; i++) {
+        if(color.options[i].value === color.value) {
+            color.remove(i);
+            break;
+        }
+    }
 }
